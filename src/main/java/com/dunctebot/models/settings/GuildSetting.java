569f4b01dda3f0bddaf74d5996ae13d8ec6e4fba
duncte123 <contact@duncte123.me>
2021-03-15 14:41:54 +0100

    Fix member logging

--- src/main/java/com/dunctebot/models/settings/GuildSetting.java
+++ src/main/java/com/dunctebot/models/settings/GuildSetting.java
@@ -45,7 +45,7 @@ import static com.dunctebot.models.utils.Utils.ratelimmitChecks;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GuildSetting {
     public static String DEFAULT_PREFIX = "db!";
-    public static final String[] LOGGING_TYPES = {"Invite", "Ban", "Unban", "Mute", "Kick", "Warn"};
+    public static final String[] LOGGING_TYPES = {"Invite", "Member", "Ban", "Unban", "Mute", "Kick", "Warn"};
 
     private final long guildId;
     private List<String> blacklistedWords = new ArrayList<>();
